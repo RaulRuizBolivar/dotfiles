3703b642291437ce9f5c29b7d20564bbf794dc8a
Raul Ruiz Bolivar <raul.ruiz.bolivar@gmail.com>
2024-09-19 19:05:47 +0200

    added gruvbox themes and change current-theme for an include of the selected theme

--- zshrc/alias.sh
+++ zshrc/alias.sh
@@ -8,9 +8,12 @@
 
 alias n="nvim"
 alias v="nvim"
+alias r="ranger"
 alias zrc="nvim ~/.zshrc"
 alias dev="npm run start:dev"
 alias lgit="lazygit"
 alias refresh="source ~/.zshrc"
 alias zellij="/opt/zellij/zellij"
 alias zll="zellij a work"
+alias steam="prime-run flatpak run com.valvesoftware.Steam &"
+alias zen="prime-run flatpak run io.github.zen_browser.zen &"
